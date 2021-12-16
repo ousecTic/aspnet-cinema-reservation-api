7786f9ff2670cf471dbe51b114e5777fcc6981b2
Henry Ly <henryly213@gmail.com>
2021-12-16 11:58:18 -0600

    completed project

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AuthenticationPlugin;
+using CinemaWebApi.Data;
+using CinemaWebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace CinemaWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+        private IConfiguration _configuration;
+        private readonly AuthService _auth;
+
+
+        public UsersController(CinemaDbContext dbContext, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _auth = new AuthService(_configuration);
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody] User user)
+        {
+            var userWithSameEmail = _dbContext.Users.Where(u => u.Email == user.Email).SingleOrDefault();
+
+            if(userWithSameEmail != null)
+            {
+                return BadRequest("User already exist!");
+            }
+
+            var userObj = new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = SecurePasswordHasherHelper.Hash(user.Password),
+                Role = user.Role
+            };
+
+            _dbContext.Users.Add(userObj);
+            _dbContext.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] User user)
+        {
+            var userEmail = _dbContext.Users.FirstOrDefault(u => u.Email == user.Email);
+
+            if(userEmail == null)
+            {
+                return NotFound();
+            }
+
+            if (!SecurePasswordHasherHelper.Verify(user.Password, userEmail.Password))
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, userEmail.Role)
+            };
+
+            var token = _auth.GenerateAccessToken(claims);
+
+            return new ObjectResult(new
+            {
+                access_token = token.AccessToken,
+                expires_in = token.ExpiresIn,
+                token_type = token.TokenType,
+                creation_Time = token.ValidFrom,
+                expiration_Time = token.ValidTo,
+                user_id = userEmail.Id
+            }) ;
+        }
+
+        
+    }
+}
---
+++ Data/CinemaDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CinemaWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaWebApi.Data
+{
+    public class CinemaDbContext : DbContext
+    {
+        //dbcontext is what applies CRUD operation
+
+        //thedbcontextoptions contains the database provider to use, connection strings , and any other DB related config information
+
+        public CinemaDbContext(DbContextOptions<CinemaDbContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
